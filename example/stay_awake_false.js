23d6a30b711463ccea1e3ac18cdc780e6c6ffe96
CitizenOneX <17427385+CitizenOneX@users.noreply.github.com>
2025-05-17 19:22:32 +1000

    added missing break

--- example/stay_awake_false.js
+++ example/stay_awake_false.js
@@ -6,6 +6,9 @@ export async function run() {
   // Connct to Frame
   const deviceId = await frameBle.connect();
 
+  // Send a break signal to Frame in case it is in a loop/main.lua
+  await frameBle.sendBreakSignal();
+
   // Restore normal behavior that Frame turns off when placed in the charging cradle (and puts it to sleep now)
   await frameBle.sendLua("frame.stay_awake(false);print(0)", {awaitPrint: true})
   await frameBle.sendLua("frame.sleep()")
--- example/stay_awake_true.js
+++ example/stay_awake_true.js
@@ -6,6 +6,9 @@ export async function run() {
   // Connct to Frame
   const deviceId = await frameBle.connect();
 
+  // Send a break signal to Frame in case it is in a loop/main.lua
+  await frameBle.sendBreakSignal();
+
   // Keep Frame awake even in charging cradle (for development)
   await frameBle.sendLua("frame.stay_awake(true);print(0)", {awaitPrint: true})
 
